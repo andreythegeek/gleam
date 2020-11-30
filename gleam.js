345b5af89ae3ee2a58b714efb31a3557a78cb992
Andrey Petrov <andrey@petrov.works>
2020-11-30 11:47:12 +0300

    Add colors.

---
+++ gleam.js
@@ -0,0 +1,39 @@
+black        = '#1E2320';
+red          = '#D78787';
+green        = '#60B48A';
+yellow       = '#DFAF8F';
+blue         = '#506070';
+magenta      = '#DC8CC3';
+cyan         = '#8CD0D3';
+white        = '#DCDCCC';
+lightBlack   = '#709080';
+lightRed     = '#DCA3A3';
+lightGreen   = '#C3BF9F';
+lightYellow  = '#F0DFAF';
+lightBlue    = '#94BFF3';
+lightMagenta = '#EC93D3';
+lightCyan    = '#93E0E3';
+lightWhite   = '#FFFFFF';
+
+t.prefs_.set('color-palette-overrides', [
+    black,
+    red,
+    green,
+    yellow,
+    blue,
+    magenta,
+    cyan,
+    white,
+    lightBlack,
+    lightRed,
+    lightGreen,
+    lightYellow,
+    lightBlue,
+    lightMagenta,
+    lightCyan,
+    lightWhite
+]);
+
+t.prefs_.set('cursor-color', 'rgba(0, 0, 0, 0.5)');
+t.prefs_.set('foreground-color', white);
+t.prefs_.set('background-color', '#3A3A3A');
